7f70d7a7226473a95e4c182e937abc141e8c9743
Youi-4 <nra64321@gmail.com>
2026-02-14 15:14:33 +1100

    trouble shooting why sentences are not displaying

--- src/pages/SharedSpace/SharedSpaceProvider.tsx
+++ src/pages/SharedSpace/SharedSpaceProvider.tsx
@@ -37,9 +37,10 @@ interface SharedSpaceContextType {
 
 /* ------------------ Socket URL ------------------ */
 
-const SOCKET_URL = import.meta.env.VITE_API_URL
-  ? import.meta.env.VITE_API_URL.replace(/\/api$/, "")
-  : "http://localhost:3000";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL
+  || (import.meta.env.VITE_API_URL
+    ? import.meta.env.VITE_API_URL.replace(/\/api$/, "")
+    : "http://localhost:3000");
 
 /* ------------------ Context ------------------ */
 
